c831c28d436f3032000914f1d50bc2b8f388b38c
Snorre Brandstadmoen <snorre.brandstadmoen@bekk.no>
2011-09-12 23:10:33 +0200

    dagens markup

---
+++ original/index_files/1193029.js
@@ -0,0 +1,7 @@
+document.write('<link rel="stylesheet" href="https://gist.github.com/stylesheets/gist/embed.css"/>')
+
+
+
+
+
+document.write('<div id=\"gist-1193029\" class=\"gist\">\n  \n  \n    \n      \n        \n        \n\n        <div class=\"gist-file\">\n          <div class=\"gist-data gist-syntax\">\n            \n            \n            \n              <div class=\"gist-highlight\"><pre><div class=\'line\' id=\'LC1\'>POST<\/div><div class=\'line\' id=\'LC2\'>/path/to/resource<\/div><div class=\'line\' id=\'LC3\'>content-md5: Q2hlY2sgSW50YWdyaXR5IQ==<\/div><div class=\'line\' id=\'LC4\'>date: Wed, 29 Jun 2011 14:58:11 GMT<\/div><div class=\'line\' id=\'LC5\'>x-vendor-userid: 59829<\/div><div class=\'line\' id=\'LC6\'>param1=34&amp;param2=lol<\/div><\/pre><\/div>\n            \n          <\/div>\n\n          <div class=\"gist-meta\">\n            <a href=\"https://gist.github.com/raw/1193029/9e72324f392b30ae1327db04456ea0b49898ede2/gistfile1.txt\" style=\"float:right;\">view raw<\/a>\n            <a href=\"https://gist.github.com/1193029#file_gistfile1.txt\" style=\"float:right;margin-right:10px;color:#666\">gistfile1.txt<\/a>\n            <a href=\"https://gist.github.com/1193029\">This Gist<\/a> brought to you by <a href=\"http://github.com\">GitHub<\/a>.\n          <\/div>\n        <\/div>\n      \n    \n  \n<\/div>\n')
